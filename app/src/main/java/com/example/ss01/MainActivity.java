9a8ff76f243d76100d591cd0bc35638db0e87042
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2021-05-26 12:08:26 +0700

    Android assgignment 01
    Android assgignment 01

--- app/src/main/java/com/example/ss01/MainActivity.java
+++ app/src/main/java/com/example/ss01/MainActivity.java
@@ -3,18 +3,30 @@ package com.example.ss01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo);
+        setContentView(R.layout.activity_calculator);
+
     }
+
 
     @Override
     public void onClick(View v) {
 
     }
 }
+
+
+
